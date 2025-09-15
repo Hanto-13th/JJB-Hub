0a656fcb01803c2b5c3df89050610c7e233f7522
Hanto_13th <antoninhidien@orange.fr>
2025-09-15 17:58:32 +0200

    Create the base of users accounts and login, with the templates of each page

--- JJBhub/badges/models.py
+++ JJBhub/badges/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
-class Badges(models.Model):
+#the class for each badges the users can receive with relation Many to Many for users
+class Badge(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     condition = models.CharField(max_length=200)
     point = models.IntegerField()
     obtention_date = models.DateField()
+
+    def __str__(self):
+        return self.name
 
 
 
--- JJBhub/main/views.py
+++ JJBhub/main/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 
+#the views to redirect to the page for login/create_club/create_user
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    return render(request,"main/index.html")
+
+def go_to_create_club_page(request):
+    return redirect("create_club_page")
+
+def go_to_create_account_page(request):
+    return redirect("create_account_page")
+
+def go_to_login_page(request):
+    return redirect("login_page")
+
+#after login display the user homepage
+@login_required(login_url="login/")
+def home(request,username):
+    #check if the connected user is the same display in the URL
+    if username != request.user.username:
+        return redirect(reverse("home", kwargs={"username": request.user.username}))
+    #else display his homepage
+    return render(request, "main/home.html", {"username": request.user.username})
+
+
+
